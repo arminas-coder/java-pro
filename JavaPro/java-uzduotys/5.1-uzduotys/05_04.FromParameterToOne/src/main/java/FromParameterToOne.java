81a5f07f63edaa6e50cabde90476fd8a4bc9edcb
[Arminas Jakovlevas] <[arminasjakovlevas@gmail.com]>
2025-02-24 18:55:03 +0200

    Padarytos 5.1 užduotys

--- JavaPro/java-uzduotys/5.1-uzduotys/05_04.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ JavaPro/java-uzduotys/5.1-uzduotys/05_04.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -1,10 +1,16 @@
 
 
 public class FromParameterToOne {
-
     public static void main(String[] args) {
-
-
+        // Iškviečiu metoda su skirtingais parametrais
+        printFromNumberToOne(5);
+        printFromNumberToOne(2);
     }
 
+    public static void printFromNumberToOne(int number) {
+        // For ciklas, kad spausdintų nuo 1
+        for (int i = number; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
 }
--- JavaPro/java-uzduotys/5.1-uzduotys/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ JavaPro/java-uzduotys/5.1-uzduotys/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -2,10 +2,17 @@
 import java.util.Scanner;
 
 public class DivisibleByThree {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        divisibleByThreeInRange(3, 6);
+        divisibleByThreeInRange(2, 10);
     }
 
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            // Patikrinu ar dalijasi iš 3
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
