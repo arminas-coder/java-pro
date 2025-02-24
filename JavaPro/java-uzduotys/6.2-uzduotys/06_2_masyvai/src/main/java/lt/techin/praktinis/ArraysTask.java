4d4ee8e865486a0e289da9bf2059d3baf155bc71
[Arminas Jakovlevas] <[arminasjakovlevas@gmail.com]>
2025-02-24 21:21:15 +0200

    Pridėtos 6.2 užduotys

---
+++ JavaPro/java-uzduotys/6.2-uzduotys/06_2_masyvai/src/main/java/lt/techin/praktinis/ArraysTask.java
@@ -0,0 +1,77 @@
+package lt.techin.praktinis;
+
+public class ArraysTask {
+
+
+    public static void main(String[] args) {
+        // Įgyvendikite visus metodus (turi pažaliuoti unit testai)
+        // Pateiktas masyvas ir pavyzdys programos patikrinimui. Masyvo elementų reikšmes galite keisti.
+
+        int[] arr = {3, 2, 1, 4, 6, 5};
+
+        int minMark = getMin(arr);
+        System.out.printf("Min mark: %d\n", minMark);
+
+    }
+
+    //Metodas turi grąžinti pirmą masyvo elementą
+    public static int getFirstElement(int[] arr) {
+        //TODO
+        return 0;
+    }
+
+    //Metodas turi grąžinti paskutinį masyvo elementą
+    public static int getLastElement(int[] arr) {
+        //TODO
+        return 0;
+    }
+
+    //Raskite mažiausią masyvo elementą
+    public static int getMin(int[] arr) {
+        //TODO
+        return 0;
+    }
+
+    //Raskite didžiausią masyvo elementą
+    public static int getMax(int[] arr) {
+        //TODO
+        return 0;
+    }
+
+    //Suskaičiuokite masyvo elementų sumą
+    public static int getSum(int[] arr) {
+        //TODO
+        return 0;
+    }
+
+    //Raskite masyvo elementų vidurkį
+    public static int getAverage(int[] arr) {
+        //TODO
+        return 0;
+    }
+
+
+    //Suskaičiuokite kiek masyve yra skaičių, didesnių nei duotas skaičius n
+    public static int countElements(int[] arr, int n) {
+        //TODO
+        return 0;
+    }
+
+    //Metodas turi grąžinti true, jei masyve yra elementas kurio reikšmė lygi n
+    public static boolean contains(int[] arr, int n) {
+        //TODO
+        return false;
+    }
+
+    //Raskite elemento kurio reikšmė lygi n paskutinį indeksą
+    public static int lastIndexOf(int[] arr, int n) {
+        //TODO
+        return 0;
+    }
+
+
+
+
+
+
+}
